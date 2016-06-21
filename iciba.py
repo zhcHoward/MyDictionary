e6e4216542d22e5f39d42490cd8f79b47af7bee7
Howard Zhu <zhc_howard@hotmail.com>
2016-06-21 17:54:16 +0800

    get the word from command line instead of inputing later

--- iciba.py
+++ iciba.py
@@ -1,29 +1,30 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import requests
 import bs4
 
+if __name__ == '__main__':
+    url = 'http://www.iciba.com/'
+    word = sys.argv[1]
 
-url = 'http://www.iciba.com/'
-word = input('Please input a word: ')
+    response = requests.get(url + word)
+    html = response.text
+    soup = bs4.BeautifulSoup(html, 'html5lib')
 
-response = requests.get(url + word)
-html = response.text
-soup = bs4.BeautifulSoup(html, 'html5lib')
+    word = soup.find('h1', 'keyword').text
+    explains_html = soup.find(attrs={'class': 'base-list'})
+    explains = {}
+    for each_prop in explains_html.find_all(attrs={'class': 'clearfix'}):
+        # result = each_prop.find(attrs={'class': 'clearfix'})
+        explains.update({
+            each_prop.find('span', 'prop').text: each_prop.find('p').text.split()
+        })
 
-word = soup.find('h1', 'keyword').text
-explains_html = soup.find(attrs={'class': 'base-list'})
-explains = {}
-for each_prop in explains_html.find_all(attrs={'class': 'clearfix'}):
-    # result = each_prop.find(attrs={'class': 'clearfix'})
-    explains.update({
-        each_prop.find('span', 'prop').text: each_prop.find('p').text.split()
-    })
-
-print(word + ':')
-for prop, detail in explains.items():
-    print(prop + '\t', end='')
-    for d in detail:
-        print(d + ' ', end='')
-    print()
+    print(word + ':')
+    for prop, detail in explains.items():
+        print(prop + '\t', end='')
+        for d in detail:
+            print(d + ' ', end='')
+        print()
