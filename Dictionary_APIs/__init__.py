5b8d5ee5640ad6f6c87b7da5c026835289ef5b15
Howard Zhu <zhc_howard@hotmail.com>
2016-06-22 18:20:25 +0800

    bug fixes

---
+++ Dictionary_APIs/__init__.py
@@ -0,0 +1,1 @@
+from . import iciba, youdao
--- dictionary.py
+++ dictionary.py
@@ -7,21 +7,38 @@ from Dictionary_APIs.iciba import Iciba
 from Dictionary_APIs.youdao import Youdao
 
 
+dictionaries = {
+    'iciba': Iciba,
+    'youdao': Youdao,
+}
+
+
 class DictionaryAPI(object):
     """docstring for DictionaryAPI"""
 
     def __init__(self, word='', service=Youdao):
         self.service = service(word)
 
-    def search(self, word):
-        self.service.search(word)
+    def search(self, word=''):
+        if word:
+            self.service.search(word)
+        else:
+            self.service.search()
 
     def display(self):
         self.service.display()
 
 
 if __name__ == '__main__':
-    word = sys.argv[1]
-    dictionary = DictionaryAPI()
-    dictionary.search(word)
+    if len(sys.argv) == 3:
+        kwargs = {
+            'word': sys.argv[1],
+            'service': dictionaries.get(sys.argv[2])
+        }
+    else:
+        kwargs = {
+            'word': sys.argv[1],
+        }
+    dictionary = DictionaryAPI(**kwargs)
+    dictionary.search()
     dictionary.display()
