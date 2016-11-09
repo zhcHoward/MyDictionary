1f4b611896317022d2f4b43313fbc53fcd8a06cc
Howard Zhu <zhc_howard@hotmail.com>
2016-11-09 09:48:46 +0800

    format code

--- Dictionary_APIs/base.py
+++ Dictionary_APIs/base.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 
@@ -36,4 +36,3 @@ class DictionaryBase(object):
             for d in detail:
                 print(d + ' ', end='')
             print()
-
--- dictionary.py
+++ dictionary.py
@@ -54,4 +54,3 @@ if __name__ == '__main__':
     dictionary = DictionaryAPI(**kwargs)
     dictionary.search()
     dictionary.display()
-
